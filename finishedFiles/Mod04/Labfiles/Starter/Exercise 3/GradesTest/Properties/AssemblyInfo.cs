01dd799fed33c7d23588d5f4b8e8b7bfd3b367e7
WilliamDWesp <williamdwesp@gmail.com>
2021-02-23 13:56:04 -0800

    Mod3, Mod4, Mod5
    finsihed my version of Mod3, Mod4, Mod5

---
+++ finishedFiles/Mod04/Labfiles/Starter/Exercise 3/GradesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GradesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GradesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6fc3d411-ade8-4a61-965d-c17b82436554")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
