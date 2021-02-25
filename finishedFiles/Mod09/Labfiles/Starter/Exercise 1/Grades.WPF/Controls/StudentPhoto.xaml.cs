763cadb499630f62c7228dcb1413abc7744c3c5a
WilliamDWesp <williamdwesp@gmail.com>
2021-02-25 12:38:29 -0800

    my version of mod 9 and mod 10

---
+++ finishedFiles/Mod09/Labfiles/Starter/Exercise 1/Grades.WPF/Controls/StudentPhoto.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Grades.WPF
+{
+    /// <summary>
+    /// Interaction logic for StudentPhoto.xaml
+    /// </summary>
+    public partial class StudentPhoto : UserControl
+    {
+        public StudentPhoto()
+        {
+            InitializeComponent();
+        }
+    }
+}
